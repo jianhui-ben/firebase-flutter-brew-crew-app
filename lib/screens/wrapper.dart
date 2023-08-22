6cf4373a3f001c4b3acbfc32ae9ac44cf05741de
Jianhui Ben <jianhui.ben@gmail.com>
2023-08-21 22:37:26 -0700

    add basic app structure

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_flutter_brew_crew_app/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    //return either Home or Authenticate widget
+    return Home();
+  }
+}
