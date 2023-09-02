e1687ab76fd08626ae7f1c737cb4f89316d9d2d4
Jianhui Ben <jianhui.ben@gmail.com>
2023-09-02 16:48:04 -0700

    add slider for coffee strength

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_flutter_brew_crew_app/shared/constants.dart';
 
@@ -79,6 +81,37 @@ class _SettingsFormState extends State<SettingsForm> {
 
             const SizedBox(
               height: 20,
+            ),
+
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SliderTheme(
+                  data: SliderTheme.of(context).copyWith(
+                    activeTrackColor: Colors.brown[_curStrength!],
+                    inactiveTrackColor: Colors.grey,
+                    trackHeight: 4.0,
+                    thumbColor: Colors.brown[_curStrength!],
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 12.0),
+                    overlayShape: RoundSliderOverlayShape(overlayRadius: 20.0),
+                  ),
+                  child: Slider(
+                    value: (_curStrength ?? 100).toDouble() ,
+                    min: 100.0,
+                    max: 900.0,
+                    divisions: 8,
+                    onChanged: (value) {
+                      setState(() {
+                        _curStrength = value.round(); // Update selected number
+                      });
+                    },
+                  ),
+                ),
+                Text(
+                  'Selected Number: ${_curStrength?.toStringAsFixed(0)}',
+                  style: TextStyle(fontSize: 15),
+                ),
+              ],
             ),
 
             //slider to select strength
